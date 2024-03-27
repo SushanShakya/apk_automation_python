be04d471cd2e13c67b9289016bea5025b7e8bc9d
SushanShakya <sushaanshakya88@gmail.com>
2024-03-27 13:25:40 +0545

    Feat: Send Messages to Discord

--- daily_update_automation.py
+++ daily_update_automation.py
@@ -1,62 +1,55 @@
-import discord
-import asyncio
 import json
+import requests
+import termcolor
+from halo import Halo
 
-# Specify the path to your JSON file
-path_to_token = 'discord_token.json'
-
-# Open the JSON file and load its contents
-with open(path_to_token, 'r') as file:
-    data = json.load(file)
-
-TOKEN = data['token']
-SERVER_ID = "1007198475642994768"
-CHANNEL_ID = "1120657824947634317"
 THREAD_ID = "1171697957775609937"
+TOKEN_FILE = 'discord_token.json'
 
-# Initialize bot
-client = discord.Client()
+spinner_config = {
+    "interval": 80,
+		"frames": [
+			"⠋",
+			"⠙",
+			"⠹",
+			"⠸",
+			"⠼",
+			"⠴",
+			"⠦",
+			"⠧",
+			"⠇",
+			"⠏"
+		]
+}
 
-@client.event
-async def on_ready():
-    print(f'Logged in as {client.user.name}')
+def authenticate():
+    with open(TOKEN_FILE, 'r') as f:
+            token_data = json.load(f)
+    return token_data.get('access_token')
 
-async def send_message(thread_id: int, channel_id: int, server_id: int, message: str):
-    try:
-        # Fetch the server
-        server = await client.fetch_guild(server_id)
-        if server is None:
-            print(f"Server with ID {server_id} not found.")
-            return
-        
-        # Fetch the channel
-        channel = await server.fetch_channel(channel_id)
-        if channel is None:
-            print(f"Channel with ID {channel_id} not found in server {server.name}.")
-            return
+@Halo(text="Updating Message", spinner=spinner_config)
+def send_message(access_token, thread_id, message_content):
+    headers = {
+        'Authorization': f'{access_token}',
+        'Content-Type': 'application/json'
+    }
+    send_message_url = f'https://discord.com/api/v9/channels/{thread_id}/messages'
+    message_payload = {
+        'content': message_content,
+        'thread_id': thread_id
+    }
+    response = requests.post(send_message_url, headers=headers, json=message_payload)
+    return response
+    
 
-        # Fetch the thread
-        thread = await channel.fetch_thread(thread_id)
-        if thread is None:
-            print(f"Thread with ID {thread_id} not found in channel {channel.name}.")
-            return
+def main():
+    access_token = authenticate()
+    if access_token:
+        message_content = "TEST MESSAGE"
+        response = send_message(access_token, THREAD_ID, message_content)
+        if response.status_code == 200:
+            termcolor.cprint('\u2713 Message sent successfully','green')
+        else:
+            termcolor.cprint(f'Failed to send message. Status code: {response.status_code}, Error: {response.text}','red')
 
-        # Send the message to the thread
-        await thread.send(message)
-        print(f"Message sent to thread {thread_id} in channel {channel.name}.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-
-# Run the bot
-async def main():
-    await client.start(TOKEN)
-    await send_message(
-        thread_id=THREAD_ID, 
-        channel_id=CHANNEL_ID, 
-        server_id=SERVER_ID, 
-        message="Test Message"
-    )
-    await asyncio.sleep(5)  # Wait for 5 seconds before closing the connection
-    await client.close()
-
-asyncio.run(main())
+main()
